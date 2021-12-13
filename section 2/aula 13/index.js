7f38b29cfc4e1d0d25925c770f4787c62750da29
Paulo Vítor <paulovmedeiros@outlook.com>
2021-12-13 14:09:21 -0300

    aula 13 - variáveis com let

---
+++ section 2/aula 13/index.js
@@ -0,0 +1,29 @@
+// Variáveis em JavaScript
+
+/* Varíaveis são utilizadas para guardar informações que
+ * podem ser alteradas. Essas informações podem ser de
+ * diferentes tipos, como textos ou números.
+ * Em JavaScript, podemos criamos variáveis da seguinte forma:
+ */
+
+let nome = 'João'; // Criando e inicializando a variável
+
+console.log(nome);
+
+/* Usamos let ou var para informar que estamos criando uma variável.
+ * Logo em seguida atribuimos um rótulo a ela, nesse caso é
+ * 'nome', e atribuímos um valor para ela: a string 'João'.
+ * Também podemos declarar variáveis sem inicializar (atribuir
+ * um valor). Por exemplo:
+ */
+
+let sobrenome; // Criando a variável sem inicializar
+
+/* Podemos mudar ou atribuir um valor a uma variável a
+ * qualquer momento. Exemplo:
+ */
+
+sobrenome = 'Bezerra'; // Atribuindo valor à variável sobrenome
+
+console.log(sobrenome);
+
