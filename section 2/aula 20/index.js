4f4058155dc00b02095f94116c6fa7534a63655b
Paulo Vítor <paulovmedeiros@outlook.com>
2021-12-21 14:02:31 -0300

    aula 20 - operadores aritméticos

---
+++ section 2/aula 20/index.js
@@ -0,0 +1,40 @@
+// Operadores Aritméticos
+
+// + => Realiza adição e concatenação de valores. Exemplo:
+const num1 = 5;
+const num2 = 10;
+console.log(num1 + num2);
+
+// Quando aplicamos o + com tipos diferentes de dados, ele realiza a concatenação desses valores:
+const num3 = "5";
+const num4 = 10;
+console.log(num3 + num4);
+
+// - => Realiza a subtração entre dois valores numéricos:
+console.log(num2 - num1);
+
+// * => Realiza a multiplicação entre dois valores numéricos:
+console.log(num1 * num2);
+
+// / => Realiza a divisão entre dois valores numéricos:
+console.log(num2 / num1);
+
+// ** => Realiza a potenciação entre dois números:
+console.log(num1 ** 2);
+
+// % => Retorna o resto da divisão entre dois números:
+console.log(num1 % 3);
+
+// ++ => Incrementa em 1 o valor de uma variável numérica:
+num1++;
+console.log(num1);
+
+// -- => Decrementa em 1 o valor de uma variável numérica:
+num2--;
+console.log(num2);
+
+// += -= *= => Atribuem ao valor da variável a operação desejada com um número ou variável:
+num3 += 2;
+num3 -= 2;
+num3 *= 2;
+
